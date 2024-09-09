f59f8bd94b8b850eef32cc1ee64f456df672b43d
juannpg <juan.pasgut.workspace@gmail.com>
2024-09-09 15:35:21 +0200

    frontend now propperly checks if the user is admin and shows the page according to the response

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,12 +1,50 @@
-import type { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "Twitter Clone | Admin Dashboard",
-};
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function AdminPage() {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  async function checkAdmin() {
+    const token = localStorage.getItem('token');
+    
+    const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+    const response = await fetch(`${apiBaseUrl}/api/verifyAdmin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: token,
+      })
+    });
+  
+    if (!response.ok) {
+      setIsAdmin(false);
+      return;
+    }
+    setIsAdmin(true);
+  }
+
+  useEffect(() => {
+    checkAdmin();
+  }, []);
+
   return (
-    <main className='h-screen flex flex-col items-center justify-center'>
-      <h1 className='text-4xl mb-1'>Admin Dashboard</h1>
-    </main>
+    <>
+      {isAdmin == true ? (
+        <main className='h-screen flex flex-col items-center justify-center'>
+          <h1 className='text-4xl mb-1'>Admin Dashboard</h1>
+          <div>
+            <h2>bla bla bla admin routers bla bla bla</h2>
+          </div>
+        </main>
+      ) : (
+        <main className='h-screen flex flex-col items-center justify-center'>
+          <h1 className='text-4xl mb-1'>Admin Dashboard</h1>
+          <p className='text-text mb-1'>You are not an admin</p>
+        </main>
+      )}
+    </>
   );
 }
--- frontend/src/components/AdminPageBtnComp.tsx
+++ frontend/src/components/AdminPageBtnComp.tsx
@@ -1,33 +1,10 @@
-"use client";
-
-const checkAdmin = async () => {
-  const token = localStorage.getItem('token');
-  
-  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-  const response = await fetch(`${apiBaseUrl}/api/verifyAdmin`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      token: token,
-    })
-  });
-
-  if (!response.ok) {
-    alert("user is not admin");
-    return;
-  }
-  window.location.href = "/admin";
-}
-
 export default function AdminPageBtn() {
 
   return(
-    <button
+    <a
       className='mt-2 border-black bg-opacity-60 bg-black px-3 py-2 border-none rounded-md text-text hover:bg-secondary hover:border-primary hover:rounded-md hover:scale-125 transition mb-3'
-      onClick={checkAdmin}
+      href="/admin"
       ><strong>Admin Dashboard</strong>
-    </button>
+    </a>
   )
 }
